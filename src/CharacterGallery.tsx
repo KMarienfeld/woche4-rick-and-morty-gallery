31485e7cae284cf444bfed4150371bfab8be1b04
Kristina <kristina.marienfeld@gmail.com>
2023-05-11 20:30:16 +0200

    add aufg  filterfunktion

--- src/CharacterGallery.tsx
+++ src/CharacterGallery.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
 import allCharacters from './allCharacters.json'
 import CharacterCard from "./CharacterCard";
 
 
 
 function CharacterGallery() {
+    const charListe = allCharacters
+    const [inputFieldValue, setInputFieldValue] = useState("");
+    function useTextInput(event: ChangeEvent<HTMLInputElement>) {
+        setInputFieldValue(event.target.value)
+    }
+
+        const filteredList = charListe.filter((character) => character.status.includes(inputFieldValue))
+
+
+
     return (
         <div className="App">
-            {allCharacters.map((character) => <CharacterCard key = {character.id} name={character.name} status={character.status} image={character.image}/>)}
+        <input type="text" value={inputFieldValue} placeholder="Trage hier einen Status ein"
+        onChange={useTextInput}/>
+        <br/>
+            {filteredList.map((character) => <CharacterCard key = {character.id} name={character.name} status={character.status} image={character.image}/>)}
         </div>
     )
 
